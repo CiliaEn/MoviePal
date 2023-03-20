64b802429cf8584e21b42e5a27905483fc205ee9
ciliaence <cilia.ence@telia.com>
2023-03-20 12:19:43 +0100

    init

---
+++ MoviePal/MoviePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePalApp.swift
+//  MoviePal
+//
+//  Created by Cilia Ence on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct MoviePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
